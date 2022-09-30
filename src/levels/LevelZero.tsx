a35c1de747c276bf3629004d5783424ccf783be6
Criheacy <hekaiqi@mail.sdu.edu.cn>
2022-10-01 01:03:25 +0800

    feat: Add user info validation

--- src/levels/LevelZero.tsx
+++ src/levels/LevelZero.tsx
@@ -6,8 +6,8 @@ import {
   LevelText,
   LevelTitle,
 } from "../components/common";
-import {useNavigate} from "react-router";
-import {useState} from "react";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 import useUser from "../components/useUser";
 
 const LevelZero = () => {
@@ -18,6 +18,16 @@ const LevelZero = () => {
   const [username, setUsername] = useState("");
 
   const onNextLevel = () => {
+    // validation
+    if (
+      isNaN(+userId) ||
+      +userId <= 0 ||
+      +userId >= 1e12 ||
+      username.length > 10
+    ) {
+      alert("请填写正确的用户名和学号！");
+      return;
+    }
     setUser({
       id: userId,
       username: username,
@@ -27,7 +37,11 @@ const LevelZero = () => {
 
   return (
     <FormContainer>
-      <LevelTitle text={"填写你的信息开始游戏"} stage={0}/>
+      <LevelTitle text={"填写你的信息开始游戏"} stage={0} />
+      <LevelBody>
+        <LevelText text={"请确认填写正确的信息，将按照此信息计算个人得分"} />
+        <LevelText text={"信息仅用于记录过关得分"} />
+      </LevelBody>
       <LevelBody>
         <LevelInput
           placeholder={"请输入学号"}
@@ -39,9 +53,8 @@ const LevelZero = () => {
           value={username}
           onChange={(event) => setUsername(event.target.value)}
         />
-        <LevelText text={"信息仅用于记录过关得分"}/>
       </LevelBody>
-      <LevelButton text={"前往第一关"} onClick={onNextLevel}/>
+      <LevelButton text={"前往第一关"} onClick={onNextLevel} />
     </FormContainer>
   );
 };
